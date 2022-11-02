1b8df82a00eb177f10586a3de149ba070c20d90f
Your Name <you@example.com>
2022-11-02 17:52:48 +0500

    lesson 6 complete

---
+++ Es6 # 6:undefined vs not defined in JS/index.js
@@ -0,0 +1,23 @@
+//Not defined vs undefined
+/*
+jb bi in dono ki baat hogi tu inka relation memory space sa ha
+
+Agar for e.g mana ak variable declare kiya line no 3 pa lakin uska console line no 2 pa la liya tu ya undefined
+aye ga lakin agar mana ak variable declare hi nhi kiya aur console la liya tu wo notdefined honga
+*/
+
+console.log(a)
+if(a === undefined){
+
+    console.log("variable is undefined")
+}
+
+else{
+    console.log("variable not defined")
+}
+
+var a = 'shakeeb'
+console.log(a)
+
+var a = 4
+console.log(a)
