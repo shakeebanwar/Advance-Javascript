3039a7aeaca3f2e12a8ef999ba47c4823053ccc9
Your Name <you@example.com>
2022-12-16 16:04:23 +0500

    lesson 5 complete

---
+++ Es6# 15:Throttling in JavaScript/index.js
@@ -0,0 +1,25 @@
+const mythort = (fn,d)=>{
+  
+    
+    return function(){
+        document.getElementById('search').disabled = true;
+        console.log('api call......')
+        setTimeout(()=>{
+            fn()
+        },d)
+    }
+}
+
+// function myfunc(){
+
+//     mythort()
+
+// }   
+
+const myfunc = mythort(()=>{
+
+    
+    document.getElementById('search').disabled = false
+    
+
+},3000)
